17ed5ace2be015f257700cb60d7917146f2dc588
Samuel <samuel@148.fr>
2018-08-13 16:13:16 +0200

    Add ping debug button

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,84 +1,80 @@
 <template>
-    
-<div>
-    
+  <div>
     <form id="form-login" class="fixed-middle" method="POST" action="" @submit.prevent="onSubmit" @keydown="form.errors.clear()">
-        
-        <h1>Bonjour !</h1>
+      <h1>Bonjour !</h1>
 
-        <div class="form-group">
-            <label for="email" class="">E-Mail</label>
-            
-            <input id="email" type="email" class="input" name="email" v-model="form.email">  
+      <div class="form-group">
+        <label for="email" class="">E-Mail</label>
+        <input id="email" type="email" class="input" name="email" v-model="form.email">
+      </div>
 
-        </div>
+      <div class="form-group">
+        <label for="password" class="">Mot de passe</label>
+        <input type="password" id="password" name="password" class="input" v-model="form.password">
+      </div>
 
-        <div class="form-group">
-            <label for="password" class="">Mot de passe</label>
-            
-            <input type="password" id="password" name="password" class="input" v-model="form.password">
+      <span class="error" v-if="form.errors.has('error')" v-text="form.errors.get('error')"></span>
 
-        </div>
-
-        <span class="error" v-if="form.errors.has('error')" v-text="form.errors.get('error')"></span>
-
-        <div class="form-group">
-            <button class="button" :disabled="form.errors.any()">Se connecter</button>
-           
-        </div>
-
+      <div class="form-group">
+        <button class="button" :disabled="form.errors.any()">Se connecter</button>
+      </div>
     </form>
-</div>
 
+    <div class="form-group">
+      <button class="button" v-on:click="ping">Ping</button>
+    </div>
+  </div>
 </template>
 
 <script>
-    import Form from '../helpers/form/Form.js';
-    export default {
-        data(){
-            return {
-                form: new Form({
-                    email : '',
-                    password : ''
-                }),
-                shared:store
+  import Form from '../helpers/form/Form.js';
+  export default {
+    data() {
+      return {
+        form: new Form({
+          email: '',
+          password: ''
+        }),
+        shared: store
+      }
+    },
+    methods: {
+      onSubmit() {
+        this.form.post(appURL + '/api/login')
+          .then((response) => {
+            for(let field in response){
+              this.shared[field] = response[field];
             }
-        },
-        methods : {
-        
-            onSubmit() {
-                
-                this.form.submit('post',appURL+'/api/login')
-                    .then((response) => {
-
-                        for(let field in response){
-                            // console.log(response);
-                            this.shared[field] = response[field];
-                        }
-                        this.shared.authenticated = true;
-                        local.save();
-
-                    })
-                    .catch((err) => {
-                        
-                        
-                    });
-            }
-            
-        }
+            this.shared.authenticated = true;
+            local.save();
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
+      ping: function (event) {
+        const AuthStr = 'Bearer '.concat(store.user.api_token)
+        const url = appURL + '/api/ping'
+        axios['get'](url, null, {headers: {Authorization: AuthStr}})
+          .then(response => {
+            console.log(response.data);
+          })
+          .catch(error => {
+            console.log(error);
+          })
+      }
     }
+  }
 </script>
 
 <style lang="scss">
-    
-#form-login{
+  #form-login{
     width: 60%;
     h1{
-        margin-bottom:50px;
+      margin-bottom:50px;
     }
     label{
-        text-transform: uppercase;
+      text-transform: uppercase;
     }
-}
-
+  }
 </style>
